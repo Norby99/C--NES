d8b9222442f1ea91ae89b09d8060a74db6123304
Norbert Gabos <dont.vip@gmail.com>
2024-02-17 13:38:13 +0100

    add opcodes

---
+++ CSharp-NES/Hardware/CPU/Opcodes.cs
@@ -0,0 +1,292 @@
+﻿namespace CSharp_NES.Hardware.CPU
+{
+    internal class Opcodes
+    {
+        // Opcodes
+        public byte ADC()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte AND()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte ASL()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte BCC()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte BCS()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte BEQ()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte BIT()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte BMI()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte BNE()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte BPL()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte BRK()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte BVC()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte BVS()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte CLC()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte CLD()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte CLI()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte CLV()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte CMP()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte CPX()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte CPY()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte DEC()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte DEX()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte DEY()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte EOR()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte INC()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte INX()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte INY()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte JMP()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte JSR()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte LDA()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte LDX()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte LDY()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte LSR()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte NOP()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte ORA()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte PHA()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte PHP()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte PLA()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte PLP()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte ROL()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte ROR()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte RTI()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte RTS()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte SBC()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte SEC()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte SED()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte SEI()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte STA()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte STX()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte STY()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte TAX()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte TAY()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte TSX()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte TXA()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte TXS()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte TYA()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Illigal Opcodes
+        public byte XXX()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
