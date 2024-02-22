348e89dd0bba501c1db5a44d6675fa709b9cab87
Norbert Gabos <dont.vip@gmail.com>
2024-02-22 18:09:15 +0100

    add instruction struct

---
+++ CSharp-NES/Hardware/CPU/Instruction.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp_NES.Hardware.CPU
+{
+    internal struct Instruction
+    {
+        String name;
+        Func<byte> opcode;
+        Func<byte> addressmode;
+        byte cycles = 0;
+
+        public Instruction(String name, Func<byte> opcode, Func<byte> addrmode, byte cycles)
+        {
+            this.name = name;
+            this.opcode = opcode;
+            this.addressmode = addrmode;
+            this.cycles = cycles;
+        }
+    }
+}
