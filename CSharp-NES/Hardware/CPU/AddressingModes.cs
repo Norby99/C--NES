bcbce8ee0ccdb367e419c94c2b4148a2f78900a0
Norbert Gabos <dont.vip@gmail.com>
2024-03-19 18:36:37 +0100

    implemented addressing modes

--- CSharp-NES/Hardware/CPU/AddressingModes.cs
+++ CSharp-NES/Hardware/CPU/AddressingModes.cs
@@ -18,62 +18,143 @@ namespace CSharp_NES.Hardware.CPU
         // Addressing Modes
         public byte IMP()
         {
-            throw new NotImplementedException();
+            IV.Fetched = RG.A;
+            return 0;
         }
 
         public byte IMM()
         {
-            throw new NotImplementedException();
+            IV.AddrAbs = RG.PC++;
+            return 0;
         }
 
         public byte ZP0()
         {
-            throw new NotImplementedException();
+            IV.AddrAbs = CPU.Read(RG.PC);
+            RG.PC++;
+            IV.AddrAbs &= 0x00FF;
+            return 0;
         }
 
         public byte ZPX()
         {
-            throw new NotImplementedException();
+            IV.AddrAbs = (ushort)(CPU.Read(RG.PC) + RG.X);
+            RG.PC++;
+            IV.AddrAbs &= 0x00FF;
+            return 0;
         }
 
         public byte ZPY()
         {
-            throw new NotImplementedException();
+            IV.AddrAbs = (ushort)(CPU.Read(RG.PC) + RG.Y);
+            RG.PC++;
+            IV.AddrAbs &= 0x00FF;
+            return 0;
         }
 
         public byte REL()
         {
-            throw new NotImplementedException();
+            IV.AddrRel = CPU.Read(RG.PC);
+            RG.PC++;
+
+            if ((IV.AddrRel & 0x80) != 0)
+            {
+                IV.AddrRel |= 0xFF00;
+            }
+
+            return 0;
         }
 
         public byte ABS()
         {
-            throw new NotImplementedException();
+            UInt16 lo = CPU.Read(RG.PC);
+            RG.PC++;
+            UInt16 hi = CPU.Read(RG.PC);
+            RG.PC++;
+
+            IV.AddrAbs = (ushort)((hi << 8) | lo);
+
+            return 0;
         }
 
         public byte ABX()
         {
-            throw new NotImplementedException();
+            UInt16 lo = CPU.Read(RG.PC);
+            RG.PC++;
+            UInt16 hi = CPU.Read(RG.PC);
+            RG.PC++;
+
+            IV.AddrAbs = (ushort)((hi << 8) | lo);
+            IV.AddrAbs += RG.X;
+
+            // returns 1 if it occured an overflow and an other clock cycle is required
+            return (byte)((IV.AddrAbs & 0xFF00) != (hi << 8) ? 1 : 0);  
         }
 
         public byte ABY()
         {
-            throw new NotImplementedException();
+            UInt16 lo = CPU.Read(RG.PC);
+            RG.PC++;
+            UInt16 hi = CPU.Read(RG.PC);
+            RG.PC++;
+
+            IV.AddrAbs = (ushort)((hi << 8) | lo);
+            IV.AddrAbs += RG.Y;
+
+            // returns 1 if it occured an overflow and an other clock cycle is required
+            return (byte)((IV.AddrAbs & 0xFF00) != (hi << 8) ? 1 : 0);
         }
+
+
+        // same as above but using indicators
 
         public byte IND()
         {
-            throw new NotImplementedException();
+            UInt16 ptrLo = CPU.Read(RG.PC);
+            RG.PC++;
+            UInt16 ptrHi = CPU.Read(RG.PC);
+            RG.PC++;
+
+            UInt16 ptr = (UInt16)((ptrHi << 8) | ptrLo);
+
+            if (ptrLo == 0x00FF)    // Simulate page boundary hardware bug
+            {
+                IV.AddrAbs = (ushort)((CPU.Read((ushort)(ptr & 0xFF00)) << 8) | CPU.Read((ushort)(ptr + 0)));
+            }
+            else    // Normal behaviour
+            {
+                IV.AddrAbs = (ushort)(CPU.Read((ushort)((ptr + 1) << 8)) | CPU.Read((ushort)(ptr + 0)));
+            }
+
+            return 0;
         }
 
         public byte IZX()
         {
-            throw new NotImplementedException();
+            UInt16 t = CPU.Read(RG.PC);
+            RG.PC++;
+
+            UInt16 lo = CPU.Read((ushort)((UInt16)(t + (UInt16)RG.X) & 0x00FF));
+            UInt16 hi = CPU.Read((ushort)((UInt16)(t + (UInt16)RG.X + 1) & 0x00FF));
+
+            IV.AddrAbs = (ushort)((hi << 8) | lo);
+
+            return 0;
         }
 
         public byte IZY()
         {
-            throw new NotImplementedException();
+            UInt16 t = CPU.Read(RG.PC);
+            RG.PC++;
+
+            UInt16 lo = CPU.Read((ushort)(t & 0x00FF));
+            UInt16 hi = CPU.Read((ushort)((t + 1) & 0x00FF));
+
+            IV.AddrAbs = (ushort)((hi << 8) | lo);
+            IV.AddrAbs += RG.Y;
+
+            // returns 1 if it occured an overflow and an other clock cycle is required
+            return (byte)((IV.AddrAbs & 0xFF00) != (hi << 8) ? 1 : 0);
         }
     }
 }
